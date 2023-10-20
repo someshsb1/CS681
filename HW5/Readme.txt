c4c63d59c8f5d3d8b4727b2837711e50e84361da
Somesh Balani <someshbalanisb@gmail.com>
2023-10-20 12:20:39 -0400

    hw5 submission

---
+++ HW5/Readme.txt
@@ -0,0 +1,10 @@
+Choose one of those datasets and implement 3 types of data processing with it.
+– https://data.boston.gov/
+
+Choose Community_Center_Pools dataset from the above City of Boston website
+Parsed the data using the code snipped provided in the hw slide
+Implemented/Performed below 3 data processing methods using Stream API to analyse the data
+    - Sorting by the Community Center Pool Names
+    - Counting the Pools in the Dorchester area only
+    - Listing all the Community Center Pools in the Roxbury Area only
+
---
+++ HW5/src/edu/umb/cs681/hw5/CommunityCenterPools.java
@@ -0,0 +1,62 @@
+package edu.umb.cs681.hw5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommunityCenterPools {
+
+    //parsing the community_center_pools.csv file using below function provided in slide 2 note07
+    public static void main(String args[]) {
+    Path path = Paths.get("Community_Center_Pools.csv");
+
+        try (Stream<String> lines = Files.lines(path)) {
+            List<List<String>> community_center_pools =
+                                                        lines.map(line ->  {
+                                                        return Stream.of(line.split(","))
+                                                        .map(value -> value.substring(0, Math.min(value.length(), 29)))
+                                                        .collect(Collectors.toList());
+                                                        })
+                                                        .collect(Collectors.toList());
+            System.out.println("Community Center Pools Parsed data: ");
+            System.out.println("");
+            for (List<String> communityPools: community_center_pools) {
+                System.out.println(communityPools);
+            }
+            System.out.println("");
+
+            //filtered community center pools to display the result for pools in the roxbury area only.
+            System.out.println("Community Center Pools in the Roxbury Area: ");
+            System.out.println("");
+            community_center_pools.stream()
+            .filter(pools -> pools.get(6).contains("Roxbury"))
+            .forEach(System.out::println);
+            System.out.println("");
+
+            //sorted community center pools by their name.
+            System.out.println("Community Center Pools Sorted by Pool Center Name: ");
+            System.out.println("");
+            System.out.println(community_center_pools.get(0)); //printing the header row as is
+            community_center_pools.stream()
+            .skip(1) //preventing the header row sorting in the sorted data result.
+            .sorted(Comparator.comparing(pools -> pools.get(8)))
+            .forEach(System.out::println);
+
+            System.out.println("");
+
+            //count of community center pools in the dorchester area only.
+            long totalPools = community_center_pools.stream()
+            .filter(pools -> pools.get(6).contains("Dorchester"))
+            .count();
+            System.out.println("Total Community Center Pools in City of Boston: " + totalPools);
+
+        } catch (IOException ex) {
+            System.out.println("Error parsing the file: " + ex.getMessage());
+        }
+    }
+}
